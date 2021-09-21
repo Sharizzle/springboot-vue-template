c9ae6caefe7342b938af3054c57090dd1b48f119
Sharif Kanaan <sharif@thekanaan.com>
2021-09-21 21:30:52 +0300

    added frontend

---
+++ src/js/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Message />
+</template>
+
+<script>
+import Message from "./components/Message.vue";
+
+export default {
+  name: "App", 
+  components: {
+    Message,
+  }, 
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/js/src/components/Message.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'Message',
+  data() {
+    return {
+      msg: 'Hello'
+    }
+  },
+  mounted() {
+    fetch("/api/messages/initial-message")
+      .then((response) => response.text())
+      .then((data) => {
+          this.msg = data;
+      });
+  }
+}
+</script>
